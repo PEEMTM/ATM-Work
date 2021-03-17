42a1540a6be67dc7aeef597272a221a6c82a2247
siriwat191260 <pan-191260@hotmail.com>
2021-03-18 04:01:06 +0700

    finish username

--- pro/ATM/ATM/Source.cpp
+++ pro/ATM/ATM/Source.cpp
@@ -17,12 +17,23 @@ int main()
     
     enum States { swelcome,slogin };
     short unsigned currentState = swelcome;
-    
+    Text username,password;
+    Font font;
+    font.loadFromFile("font/roboto/Roboto-Black.ttf");
+     username.setFont(font);
+     username.setCharacterSize(55);
+     password.setFont(font);
+     password.setCharacterSize(55);
+    string username1 = "";
+    username.setPosition(526,410);
+    string password1 = "";
+    password.setPosition(526, 600);
     
     while (window.isOpen())
     {
        
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             switch (currentState)
@@ -37,13 +48,43 @@ int main()
                 break;
 
             case slogin:
-                    Background.setTexture(x2);
-                    window.draw(Background);
-                    loginbotton1.setPosition(750, 630);
-                    window.draw(loginbotton1);
-                    break;
+                Background.setTexture(x2);
+                window.draw(Background);
+                loginbotton1.setPosition(750, 630);
+                window.draw(loginbotton1);
+ 
+                break;
+            }
+            if (loginbotton1.getGlobalBounds().contains(window.mapPixelToCoords(Mouse::getPosition(window)))) {
+                if (Mouse::isButtonPressed(Mouse::Left)) {
+                    cout << "dawdwadaw";
+                }
+            }
+            if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode < 128 && event.text.unicode != 8) {
+                    username1 += event.text.unicode;
+                    username.setString(username1);
+                }
+            }
+            if (event.key.code == sf::Keyboard::BackSpace && event.type == event.KeyPressed) {
+                if (username1.size() > 0) {
+                    username1.erase(username1.end() - 1);
+                    username.setString(username1);
+                }
+                /*
+                if (event.text.unicode == 1310) {
+                    if (event.type == sf::Event::TextEntered) {
+                        if (event.text.unicode < 128 && event.text.unicode != 8) {
+                            password1 += event.text.unicode;
+                            password.setString(password1);
+                        }
+
+                    }*/
+               }
             }
 
+            window.draw(username);
+            window.draw(password);
             window.display();
         }
     }
