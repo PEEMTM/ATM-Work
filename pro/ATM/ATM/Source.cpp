b3959fec7aadbb4f0f992c5009c63e5081699eb2
siriwat191260 <pan-191260@hotmail.com>
2021-03-18 01:30:28 +0700

    login button

--- pro/ATM/ATM/Source.cpp
+++ pro/ATM/ATM/Source.cpp
@@ -8,20 +8,43 @@ int main()
     Texture x1, x2,x3;
     x1.loadFromFile("image/Background.png");
     x2.loadFromFile("image/login.png");
-    x3.loadFromFile("image/loginbotton.png");
+    x3.loadFromFile("image/loginbotton1.png");
 
     sf::RenderWindow window(sf::VideoMode(1000, 802), "SFML works!");
 
-    Sprite Background(x1), login(x2),loginbotton(x3);
-
+    Sprite Background(x1), login(x2),loginbotton1(x3);
+    
+    
+    enum States { swelcome,slogin };
+    short unsigned currentState = swelcome;
+    
+    
     while (window.isOpen())
     {
-        window.draw(Background);
-        window.display();
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+       
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            Background.setTexture(x2);
-            loginbotton.setPosition(600,500);
+            switch (currentState)
+            {
+            case swelcome:
+                window.draw(Background);
+                if (Mouse::isButtonPressed(Mouse::Left))
+                {
+                    currentState = slogin;
+                }
+
+                break;
+
+            case slogin:
+                    Background.setTexture(x2);
+                    window.draw(Background);
+                    loginbotton1.setPosition(750, 630);
+                    window.draw(loginbotton1);
+                    break;
+            }
+
+            window.display();
         }
     }
 
