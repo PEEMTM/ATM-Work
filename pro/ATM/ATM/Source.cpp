6ae32862f8a15e5f7e76602f00dc9abbda744e9e
PEEMTM <76035600+PEEMTM@users.noreply.github.com>
2021-03-19 00:56:15 +0700

    near

--- pro/ATM/ATM/Source.cpp
+++ pro/ATM/ATM/Source.cpp
@@ -1,8 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include<iostream>
+#include<sstream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<cstdlib>
+#include<cstring>
 
 using namespace std;
 using namespace sf;
+
+string toUpperStr(string x) {
+	string y = x;
+	for (unsigned i = 0; i < x.size(); i++) y[i] = toupper(x[i]);
+	return y;
+}
 
 int main()
 {
@@ -18,10 +30,10 @@ int main()
 	x9.loadFromFile("image/exitbutton.png");
 	x10.loadFromFile("image/transaction.png");
 	x11.loadFromFile("image/depositbg.png");
-	x11.loadFromFile("image/withdrawbg.png");
-	x11.loadFromFile("image/tranferbg.png");
-	x11.loadFromFile("image/checkbg.png");
-	x11.loadFromFile("image/viewbg.png");
+	x12.loadFromFile("image/withdrawbg.png");
+	x13.loadFromFile("image/tranferbg.png");
+	x14.loadFromFile("image/checkbg.png");
+	x15.loadFromFile("image/viewbg.png");
 
 
 	sf::RenderWindow window(sf::VideoMode(1000, 802), "SFML works!");
@@ -29,21 +41,56 @@ int main()
 	Sprite Background(x1), login(x2), loginbotton1(x3), transaction(x10), exitbutton(x9), viewbutton(x8), tranferbutton(x7), checkbutton(x6), withdrawbutton(x5), depositbutton(x4);
 
 
-	enum States { swelcome, slogin, stransaction, depositbg ,withdrawbg,tranferbg,checkbg,viewbg };
+	enum States { swelcome, slogin, stransaction, depositbg, withdrawbg, tranferbg, checkbg, viewbg };
 	short unsigned currentState = swelcome;
-	Text username, password, * textPtr = &username;
+	Text username, password, moneynow, inputmoney, * textPtr = &username;
 	Font font;
 	font.loadFromFile("font/roboto/Roboto-Black.ttf");
 	username.setFont(font);
 	username.setCharacterSize(55);
 	password.setFont(font);
 	password.setCharacterSize(55);
+	moneynow.setFont(font);
+	moneynow.setCharacterSize(55);
+	inputmoney.setFont(font);
+	inputmoney.setCharacterSize(55);
 	string username1 = "";
 	username.setPosition(526, 410);
+	//string moneynow = ดึงไฟล์เงินมาใส่
+	vector<string> names;
+	vector<int> spass;
+	vector<double> moneys;
+	vector<string> stats;
+	vector<double> balances;
+
+	string text, c, key;
+	int m;
+	char name[100], stat[10];
+	double balance = 0;
+	fstream list("List.txt", ios::in);
+	while (getline(list, text)) {
+		int pass;
+		double money;
+		char ctext[100];
+		char format[] = "%[^,],%d,%f,%[^,],%f";
+		strcpy_s(ctext, text.c_str());
+		sscanf_s(ctext, format, name, &pass, &money, stat, &balance);
+		names.push_back(name);
+		spass.push_back(pass);
+		moneys.push_back(money);
+		stats.push_back(stat);
+		balances.push_back(balance);
+	}
+	list.close();
+
+	string inputmoney1 = "";
 	string password1 = "";
 	string star = "";
 	string* textbox1 = &username1;
 	password.setPosition(526, 580);
+	moneynow.setPosition(705, 420);
+	inputmoney.setPosition(705, 530);
+	inputmoney.setFillColor(Color(0, 92, 168, 255));
 
 	while (window.isOpen())
 	{
@@ -58,6 +105,7 @@ int main()
 				if (depositbutton.getGlobalBounds().contains(window.mapPixelToCoords(Mouse::getPosition(window)))) {
 					if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
 						currentState = depositbg;
+
 					}
 				}
 				if (withdrawbutton.getGlobalBounds().contains(window.mapPixelToCoords(Mouse::getPosition(window)))) {
@@ -85,9 +133,9 @@ int main()
 						window.close();
 					}
 				}
-				
+
 			}
-			
+
 			if (currentState == slogin) {
 				star = "";
 
@@ -117,12 +165,14 @@ int main()
 							textPtr->setString(*textbox1);
 						}
 					}
+					key = toUpperStr(*textbox1);
 				}
 
 				for (int i = 0; i < password1.size(); i++) {
-					star += "*";
-				}
 
+					star += "*";
+
+				}
 				password.setString(star);
 
 				if (event.key.code == sf::Keyboard::BackSpace && event.type == event.KeyPressed) {
@@ -131,58 +181,128 @@ int main()
 						textPtr->setString(*textbox1);
 					}
 				}
-
 				if (loginbotton1.getGlobalBounds().contains(window.mapPixelToCoords(Mouse::getPosition(window)))) {
 					if (Mouse::isButtonPressed(Mouse::Left)) {
 						currentState = stransaction;
 					}
 				}
 			}
-		}
+			if (currentState == depositbg) {
+				textPtr = &inputmoney;
+				textbox1 = &inputmoney1;
 
+				if (FloatRect(705.f, 420.f, 256.f, 116.f).contains(window.mapPixelToCoords(Mouse::getPosition(window)))) {
+					if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+						textbox1 = &inputmoney1;
+						textPtr = &inputmoney;
+					}
+				}
 
-		window.clear();
+				if (event.type == sf::Event::TextEntered) {
+					if (event.text.unicode >= 48 && event.text.unicode <= 57 && event.text.unicode != 8) {
+						if (textPtr == &inputmoney && textbox1->size() < 7)
+						{
+							*textbox1 += event.text.unicode;
+							textPtr->setString(*textbox1);
+						}
+					}
+				}
+				if (event.key.code == sf::Keyboard::BackSpace && event.type == event.KeyPressed) {
+					if (!textbox1->empty()) {
+						textbox1->erase(textbox1->end() - 1);
+						textPtr->setString(*textbox1);
+					}
+				}
+				//รวมเงินเก่ากับใหม่
+				if (event.key.code == sf::Keyboard::Return && event.type == event.KeyPressed) {
+					int N = sizeof(names);
+					double a;
+					cout << "---------------------------------\n\n";
+					for (int i = 0; i < N; i++) {
+						if (key == toUpperStr(names[i])) {
+							m = i;
+						}
+						a = balances[m];
+						balances[m] += atof(textbox1->c_str());
+					}
 
-		switch (currentState)
-		{
-		case swelcome:
-			window.draw(Background);
-			if (Mouse::isButtonPressed(Mouse::Left))
-			{
-				currentState = slogin;
+				}
+
 			}
-			break;
 
-		case slogin:
-			Background.setTexture(x2);
-			window.draw(Background);
-			loginbotton1.setPosition(760, 670);
-			window.draw(loginbotton1);
-			window.draw(username);
-			window.draw(password);
+			cout << star << endl;
+			window.clear();
 
-			break;
+			switch (currentState)
+			{
+			case swelcome:
+				window.draw(Background);
+				if (Mouse::isButtonPressed(Mouse::Left))
+				{
+					currentState = slogin;
+				}
+				break;
 
-		case stransaction:
-			Background.setTexture(x10);
-			window.draw(Background);
-			exitbutton.setPosition(625, 450);
-			viewbutton.setPosition(465, 650);
-			checkbutton.setPosition(625, 250);
-			tranferbutton.setPosition(0, 650);
-			withdrawbutton.setPosition(0, 450);
-			depositbutton.setPosition(0, 250);
-			window.draw(exitbutton);
-			window.draw(viewbutton);
-			window.draw(checkbutton);
-			window.draw(tranferbutton);
-			window.draw(withdrawbutton);
-			window.draw(depositbutton);
+			case slogin:
+				Background.setTexture(x2);
+				window.draw(Background);
+				loginbotton1.setPosition(760, 670);
+				window.draw(loginbotton1);
+				window.draw(username);
+				window.draw(password);
 
-			break;
+				break;
+
+			case stransaction:
+				Background.setTexture(x10);
+				window.draw(Background);
+				exitbutton.setPosition(625, 450);
+				viewbutton.setPosition(465, 650);
+				checkbutton.setPosition(625, 250);
+				tranferbutton.setPosition(0, 650);
+				withdrawbutton.setPosition(0, 450);
+				depositbutton.setPosition(0, 250);
+				window.draw(exitbutton);
+				window.draw(viewbutton);
+				window.draw(checkbutton);
+				window.draw(tranferbutton);
+				window.draw(withdrawbutton);
+				window.draw(depositbutton);
+
+				break;
+
+			case depositbg:
+				Background.setTexture(x11);
+				window.draw(Background);
+				window.draw(moneynow);
+				window.draw(inputmoney);
+				break;
+
+			case withdrawbg:
+				Background.setTexture(x12);
+				window.draw(Background);
+				break;
+
+			case tranferbg:
+				Background.setTexture(x13);
+				window.draw(Background);
+				break;
+
+			case checkbg:
+				Background.setTexture(x14);
+				window.draw(Background);
+				break;
+			}
+			Text mouseText;
+			mouseText.setPosition(window.mapPixelToCoords(Mouse::getPosition(window)));
+			mouseText.setFont(font);
+			mouseText.setCharacterSize(20);
+			stringstream ss;
+			ss << window.mapPixelToCoords(Mouse::getPosition(window)).x << " " << window.mapPixelToCoords(Mouse::getPosition(window)).y;
+			mouseText.setString(ss.str());
+			window.draw(mouseText);
+
+			window.display();
 		}
-
-
-		window.display();
 	}
 }
